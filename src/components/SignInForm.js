70099500d3f2b95e136f9dd82e6bc99172dc1781
synendra <sarpasynendra999@gmail.com>
2026-03-22 23:13:34 +0530

    implemented toggle functionality to switch between forms sign in and sign up

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,10 +1,21 @@
 import React from "react";
-
+import { useState } from "react";
 const SignInForm = () => {
+    const [isSignIn, setIsSignIn] = useState(true);
+    function toggleForm() {
+        setIsSignIn(!isSignIn);
+    }
     return (
         <div className=" p-10 bg-black/70 w-72  ">
-            <h2 className="text-3xl text-white font-bold mb-2 ">Sign In</h2>
+            <h2 className="text-3xl text-white font-bold mb-2 ">{isSignIn ? "Sign In" : "Sign Up"}</h2>
             <div className="mt-6">
+                {!isSignIn && (
+                    <input
+                        className="w-full px-4 py-3 mb-4 bg-zinc-800 outline-none focus:ring-2 focus:ring-red-600 text-white placeholder-gray-400 rounded-md text-sm"
+                        type="text"
+                        placeholder="Full Name"
+                    />
+                )}
                 <input
                     className="w-full px-4 py-3 mb-4 bg-zinc-800 outline-none focus:ring-2 focus:ring-red-600 text-white placeholder-gray-400 rounded-md text-sm"
                     type="email"
@@ -18,7 +29,7 @@ const SignInForm = () => {
             </div>
 
             <button className="w-full py-3 text-white font-semibold bg-red-600 rounded-md mt-2" type="submit">
-                Sign In
+                {isSignIn ? "Sign In" : "Sign Up"}
             </button>
             <div className="flex justify-between mt-2">
                 <div className="flex items-center">
@@ -28,8 +39,10 @@ const SignInForm = () => {
                 <p className="text-gray-400 text-sm cursor-pointer hover:underline">Need help?</p>
             </div>
             <div className="flex items-center mt-10">
-                <p className="text-zinc-500 text-xs">New to Netflix?</p>
-                <span className="text-white text-sm ml-1 hover:underline">Sign up now</span>
+                <p className="text-zinc-500 text-xs">{isSignIn ? "New to Netflix?" : "Already A Member"}</p>
+                <span className="text-white text-sm ml-1 hover:underline cursor-pointer" onClick={() => toggleForm()}>
+                    {isSignIn ? "Sign up now" : "Sign In Now"}
+                </span>
             </div>
             <p className="text-zinc-400 text-xs mt-4">
                 This page is protected by Google reCAPTCHA to ensure your're not a bot
